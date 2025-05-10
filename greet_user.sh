a85c5cbb97e0c8934b100fc35a80e47fe89db144
M R Wasik Ahmed Apon <aponwasikahmed@gmail.com>
2025-05-11 03:13:40 +0600

    Add script to greet user

---
+++ greet_user.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Take user's name as input and greet the user
+
+# Prompt the user for their name 
+echo "Please enter your name: "
+
+# Read the user's input and store into the name variable
+read name
+
+# Print a greeting message
+echo "Hello, $name"
